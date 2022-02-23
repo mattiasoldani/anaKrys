d1a220955c34edf8e7deab5fa9a02fee29e0320c
mattiasoldani <mattiasoldani93@gmail.com>
2022-02-23 17:04:12 +0100

    analysis of calibration data

--- settings/y21StormCernH2_simulated_runList.py
+++ settings/y21StormCernH2_simulated_runList.py
@@ -4,12 +4,16 @@
 nRun0 = {
     
     # PWO 1 X0
-    "220215RandomOld" : "PWO1X0_Random_Old"
-    "220216RandomOld" : "PWO1X0_Random_Old"
+    "220215RandomOld" : "PWO1X0_Random_Old",
+    "220216RandomOld" : "PWO1X0_Random_Old",
     
     # PWO 2 X0
     
     # other runs
-    "220222calib120GeV" : "directBeam_120GeV",
-    "220222calib20GeV" : "directBeam_20GeV",
+    "220222Calib120GeV" : "directBeam_120GeV",
+    "220223Calib100GeV" : "directBeam_100GeV",
+    "220223Calib80GeV" : "directBeam_80GeV",
+    "220222Calib60GeV" : "directBeam_60GeV",
+    "220223Calib40GeV" : "directBeam_40GeV",
+    "220222Calib20GeV" : "directBeam_20GeV",
 }
