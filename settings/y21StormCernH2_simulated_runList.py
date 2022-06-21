f1ffd69797b332c9c5ec62d70ba3c810bd9e3719
mattiasoldani <mattiasoldani93@gmail.com>
2022-06-21 15:42:18 +0200

    implementing crystal signal analysis

---
+++ settings/y21StormCernH2_simulated_runList.py
@@ -0,0 +1,31 @@
+# full run list
+# shape: {number: type} (all string)
+# select the run numbers or types to be opened with nRunToOpen...
+nRun0 = {
+    
+    # PWO 1 X0
+    "220215RandomOld" : "PWO1X0_Random_Old",
+    "220216RandomOld" : "PWO1X0_Random_Old",
+    "220225Random" : "PWO1X0_Random",
+    "220503Axial" : "PWO1X0_Axial",
+    "220504AxialMagnOff" : "PWO1X0_Axial_MagnetOff",  # yet to be generated
+    
+    # PWO 2 X0
+    "2204292X0Random" : "PWO2X0_Random",
+    "2204262X0Random" : "PWO2X0_Random",
+    "2205__2X0AxialMagnOff" : "PWO2X0_Axial_MagnetOff",  # yet to be generated
+    "2205052X0Random" : "PWO2X0_Random",
+    "2205052X0Axial" : "PWO2X0_Axial",
+    "2205082X0Axial" : "PWO2X0_Axial",
+    "2205102X0Axial" : "PWO2X0_Axial",
+    "2205112X0Axial" : "PWO2X0_Axial",
+    
+    # other runs
+    "220222Calib120GeV" : "directBeam_120GeV",
+    "220223Calib100GeV" : "directBeam_100GeV",
+    "220223Calib80GeV" : "directBeam_80GeV",
+    "220222Calib60GeV" : "directBeam_60GeV",
+    "220223Calib40GeV" : "directBeam_40GeV",
+    "220222Calib20GeV" : "directBeam_20GeV",
+    "220302DirectMagnOn" : "directBeam_MagnetOn",
+}
