6f8714d927058a114d330d4a0add11da0bff22aa
mattiasoldani <mattiasoldani93@gmail.com>
2022-08-04 12:33:46 +0200

    online

--- settings/y22StormCernH2_runList.py
+++ settings/y22StormCernH2_runList.py
@@ -2,5 +2,17 @@
 # shape: {number: type} (all string)
 # select the run numbers or types to be opened with nRunToOpen...
 nRun0 = {
-    "520093" : "PWOUaglio_ScanRot0",
+    "520093" : "PWOUaglio_ScanHorsaBig0",
+    "520102" : "PWOUaglio_ScanRot0",
+    "520110" : "PWOUaglio_ScanRot0",
+    "520111" : "PWOUaglio_ScanRot1",
+    "520112" : "PWOUaglio_ScanRot2",
+    "520113" : "PWOUaglio_ScanCrad0",
+    "520114" : "PWOUaglio_ScanCrad0",
+    "520115" : "PWOUaglio_ScanRot3",
+    "520116" : "PWOUaglio_ScanCrad1",
+    "520153" : "PWOUaglio_Axial",
+    "520155" : "PWOUaglio_Random",
+    
+    "520124" : "DirectBeam_120GeV",
 }
