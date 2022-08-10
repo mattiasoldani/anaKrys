6cbfd8a7667505247e7de4efabd8af41d2fbddf8
mattiasoldani <mattiasoldani93@gmail.com>
2022-08-10 17:14:48 +0200

    fixed H2-T9 bug

--- settings/y22StormCernH2_settings.py
+++ settings/y22StormCernH2_settings.py
@@ -31,27 +31,37 @@ nLinesEv = 1
 #     (0, 0), ..., (0, nCol(0)), (1,0), ..., (1, nCol(1)), ...,  (nLines, 0), ..., (nLines, nCol(nLines))
 # mandatory with ASCII/NPZ, useless with ROOT files
 asciiMap = list()
-for i in range(6): asciiMap.append("xRaw%d" % i)
-asciiMap.append("xRaw7")  # last SiBC: x & y swapped
-asciiMap.append("xRaw6")  # last SiBC: x & y swapped
-for i in range(6): asciiMap.append("nStripHit%d" % i)
-asciiMap.append("nStripHit7")  # last SiBC: x & y swapped
-asciiMap.append("nStripHit6")  # last SiBC: x & y swapped
-for i in range(6): asciiMap.append("nHit%d" % i)
-asciiMap.append("nHit7")  # last SiBC: x & y swapped
-asciiMap.append("nHit6")  # last SiBC: x & y swapped
-for i in range(2): asciiMap.append("digiBaseAPC%d" % i)
-for i in range(9): asciiMap.append("digiBaseCaloFwd%d" % i)
-asciiMap.append("digiBaseAPC2")
-for i in range(4): asciiMap.append("digiBaseEmpty%d" % i)
-for i in range(2): asciiMap.append("digiPHRawAPC%d" % i)
-for i in range(9): asciiMap.append("digiPHRawCaloFwd%d" % i)
-asciiMap.append("digiPHRawAPC2")
-for i in range(4): asciiMap.append("digiPHRawEmpty%d" % i)
-for i in range(2): asciiMap.append("digiTimeAPC%d" % i)
-for i in range(9): asciiMap.append("digiTimeCaloFwd%d" % i)
-asciiMap.append("digiTimeAPC2")
-for i in range(4): asciiMap.append("digiTimeEmpty%d" % i)
+for i in range(4): asciiMap.append("xRaw"+str(i))
+for i in range(4): asciiMap.append("nStripHit"+str(i))
+for i in range(4): asciiMap.append("nHit"+str(i))
+for i in range(16): asciiMap.append("digiBaseAll"+str(i))
+asciiMap.append("digiPHRawEmpty0")
+asciiMap.append("digiPHRawUaglio0")
+asciiMap.append("digiPHRawUaglio1")
+asciiMap.append("digiPHRawPinin1")
+asciiMap.append("digiPHRawPinin0")
+for i in range(3): asciiMap.append("digiPHRawEmpty"+str(i+1))
+asciiMap.append("digiPHRawCaloFwdBL")
+asciiMap.append("digiPHRawCaloFwdBR")
+asciiMap.append("digiPHRawCaloFwdCL")
+asciiMap.append("digiPHRawCaloFwdCC")
+asciiMap.append("digiPHRawCaloFwdCR")
+asciiMap.append("digiPHRawCaloFwdTL")
+asciiMap.append("digiPHRawCaloFwdTR")
+asciiMap.append("digiPHRawEmpty4")
+asciiMap.append("digiTimeEmpty0")
+asciiMap.append("digiTimeUaglio0")
+asciiMap.append("digiTimeUaglio1")
+asciiMap.append("digiTimePinin1")
+asciiMap.append("digiTimePinin0")
+for i in range(4): asciiMap.append("digiTimeEmpty"+str(i+1))
+asciiMap.append("digiTimeCaloFwdBL")
+asciiMap.append("digiTimeCaloFwdBR")
+asciiMap.append("digiTimeCaloFwdCL")
+asciiMap.append("digiTimeCaloFwdCC")
+asciiMap.append("digiTimeCaloFwdCR")
+asciiMap.append("digiTimeCaloFwdTL")
+asciiMap.append("digiTimeCaloFwdTR")
 asciiMap.append("xGonioRawRot")
 asciiMap.append("xGonioRawCrad")
 asciiMap.append("xGonioRawHorsa")
@@ -59,8 +69,7 @@ asciiMap.append("xGonioRawHorsaBig")
 asciiMap.append("xGonioRawVersa")
 asciiMap.append("iSpill")
 asciiMap.append("iStep")
-asciiMap.append("iEvent")
-for i in range(4): asciiMap.append("xCharge%d" % i)
+asciiMap.append("iAEv")
 
 # map of the ROOT tree variables
 # dictionary -- shape: {newName: oldName} (all string)
@@ -90,7 +99,8 @@ mirrorMap = {}
 #     and/or of all the out-of-range/in-range values for all the (lowX, upX) ranges for which bInclX=False/True
 # mandatory, but can be left empty --> no filtering
 filterMap = {}
-for i in range(4): filterMap.update({"xRaw"+str(i): [[True, [-20, 20]]]})  # senseful data from input tracking layers
+for i in range(4):
+    filterMap.update({"xRaw"+str(i): [[True, [-20, 20]]]})  # senseful data from input tracking layers
 
 ########################################################################################################################
 # SETUP GEOMETRY & TRACKING
@@ -104,14 +114,14 @@ for i in range(4): filterMap.update({"xRaw"+str(i): [[True, [-20, 20]]]})  # sen
 #     for tracking modules, use the part of the variable name following "xRaw" (base: 4/2 input/output layers)
 # mandatory, but can be skipped/filled partially for some/all runs --> all missing base positions set to 0
 z = {}
-for iRun in nRun0:
-    z.update({iRun: {
+for iVar in nRun0:
+    z.update({iVar: {
         "0": 0,
         "1": 0,
-        "2": 320,
-        "3": 320,
-        "gonio": 381,
-        "caloFwd": 381 + 95 + 40 + 615,
+        "2": 1590,
+        "3": 1590,
+        "gonio": 1590+157,
+        "caloFwd": 1590+157+29,
     }})
     
 # base tracking modules, i.e. 4 (2) in the input (output) stage
@@ -128,7 +138,7 @@ baseTrackingMap = [["0", "1", "2", "3"], ["2", "3"]]
 # mandatory for all the runs
 thInCentres = {}
 for iRun in nRun0:
-    thInCentres.update({iRun: [None, None]})
+    thInCentres.update({iRun: [-2.367236e-04, 1.050870e-04]})
     
 # raw output angle distribution centres for modules alignment
 # has to be set run by run
@@ -138,7 +148,7 @@ for iRun in nRun0:
 # mandatory for all the runs
 thOutCentres = {}
 for iRun in nRun0:
-    thOutCentres.update({iRun: [None, None]})        
+    thOutCentres.update({iRun: [0, 0]})
 
 # aligned input angle range cut, centered around 0, boundaries excluded
 # has to be set run by run
@@ -149,14 +159,14 @@ for iRun in nRun0:
 #     if length=4, the 4 values [xCutL, xCutR, yCutL, yCutR] are the boundaries of a rectangular cut
 # mandatory, but can be skipped for some/all runs --> no cut defined, i.e. boolean always True, in missing runs
 thInCut = {}
+#for iRun in nRun0:
+#    thOutCentres.update({iRun: [0, 0]})
 
 # crystal fiducial rectangle applied at the crystal longitudinal position z -- boundaries excluded
 # has to be set run by run
 # dictionary -- shape: {run (string): [xCut0, xCut1, yCut0, yCut1] (4 float)}
 # mandatory, but can be skipped for some/all runs --> no cut defined, i.e. boolean always True, in missing runs
 xCryCut = {}
-#for iRun in nRun0:
-#    xCryCut.update({iRun: [0.22, 10, -10, 10]})
 
 # upper/lower limit for low/high output multiplicity selection (included)
 # has to be set run by run
@@ -174,13 +184,12 @@ outMultCut = {}
 # pairedVar (shifted via its mean if bShift=True) is multiplied to scale and added to gonioVar
 # scale can be negative to adjust relative verso
 # mandatory, but can be left empty --> no goniometer DOF pairing
-gonioMap = {}
 gonioMap = { 
-    "Rot": ["thIn0", False, -10**6],  # TBC
-    "Crad": ["thIn1", False, 10**6],  # TBC
-    "Horsa": ["xCry0", True, -10],  # TBC
-    "HorsaBig": ["xCry0", True, 2*10],  # TBC
-}
+    "Rot": ["thIn0", False, -10**6],
+    "Crad": ["thIn1", False, 10**6],
+    "Horsa": ["xCry0", True, -10],
+    "HorsaBig": ["xCry0", True, 2*10],
+}  # careful, since run 195 Crad & HorsaBig are inverted wrt. this configuration
 
 ########################################################################################################################
 # DIGITIZERS
@@ -192,8 +201,6 @@ gonioMap = {
 # mandatory, but can be skipped for some/all runs or for some/all channels within a single run
 #     --> no cuts defined, i.e. booleans always True, in missing runs/channels
 digiPHCut = {}
-for iRun in nRun0:
-    digiPHCut.update({iRun: {}})
 
 # time cut interval -- inner events kept, boundaries excluded
 # has to be set run by run
@@ -202,6 +209,11 @@ for iRun in nRun0:
 # mandatory, but can be skipped for some/all runs or for some/all channels within a single run
 #     --> no cuts defined, i.e. booleans always True, in missing runs/channels
 digiTimeCut = {}
+for iRun in nRun0:
+    digiTimeCut.update({iRun: {}})
+    for i in range(2): digiTimeCut[iRun].update({"Uaglio%d" % i : [0, 20000]})
+    for i in range(2): digiTimeCut[iRun].update({"Pinin%d" % i : [0, 20000]})
+    for i in range(7): digiTimeCut[iRun].update({"FwdCalo%d" % i : [0, 20000]})
 
 # set of channels that are forward calorimeter channels
 # has to be set run by run
@@ -209,7 +221,11 @@ digiTimeCut = {}
 # varX format: insert the part of the variable name following "digiPHRaw"
 # mandatory, but can be skipped for some/all runs --> forward calo. total PH and energy are set to NaN for those runs
 lsDigiChCaloFwd = {}
-for iRun in nRun0: lsDigiChCaloFwd.update({iRun: ["CaloFwd"]})
+for iRun in nRun0:
+    lsDigiChCaloFwd.update({iRun: [
+        "CaloFwdBL", "CaloFwdBR", "CaloFwdTL", "CaloFwdTR",
+        "CaloFwdCL", "CaloFwdCC", "CaloFwdCR",
+    ]})
 
 # equalisation functions and parameters for channels to be equalised
 # has to be set run by run
@@ -222,7 +238,21 @@ for iRun in nRun0: lsDigiChCaloFwd.update({iRun: ["CaloFwd"]})
 # the 'end' string (within apostrophes & the precise form ", 'end'") is just a flag needed for some printing
 # mandatory, but can be skipped/filled partially for some/all runs --> raw values are kept for missing channels
 equalMap = {}
-            
+for iRun in nRun0:
+    equalMap.update({iRun: {}})
+    equalMap[iRun].update({"CaloFwdBL" : [lambda x, m, q: (x-q)/m, [59.8, -18.6], 'end']})
+    equalMap[iRun].update({"CaloFwdBR" : [lambda x, m, q: (x-q)/m, [111.8, 153.9], 'end']})
+    equalMap[iRun].update({"CaloFwdCL" : [lambda x, m, q: (x-q)/m, [160.0, 124.9], 'end']})
+    equalMap[iRun].update({"CaloFwdCC" : [lambda x, m, q: (x-q)/m, [94.212, 90.313], 'end']})
+    equalMap[iRun].update({"CaloFwdCR" : [lambda x, m, q: (x-q)/m, [101.1, -73.3], 'end']})
+    equalMap[iRun].update({"CaloFwdTL" : [lambda x, m, q: (x-q)/m, [127.4, 207.9], 'end']})
+    equalMap[iRun].update({"CaloFwdTR" : [lambda x, m, q: (x-q)/m, [101.5, 21.9], 'end']})
+    if int(iRun)>=520262:
+        equalMap[iRun].update({"Pinin0" : [lambda x, a: a*x, [0.39232], 'end']})
+        equalMap[iRun].update({"Pinin1" : [lambda x, a: a*x, [0.39232], 'end']})
+        equalMap[iRun].update({"Uaglio0" : [lambda x, a: a*x, [0.39232], 'end']})
+        equalMap[iRun].update({"Uaglio1" : [lambda x, a: a*x, [0.39232], 'end']})
+
 # (total) forward calorimeter calibration function and parameters
 # has to be set run by run
 # dictionary -- shape: {run (string): [func, param, 'end']}
@@ -233,3 +263,5 @@ equalMap = {}
 # the 'end' string (within apostrophes & the precise form ", 'end'") is just a flag needed for some printing
 # mandatory, but can be skipped for some/all runs --> forward calo. energy is set to NaN for those runs
 calibMapFwd = {}
+for iRun in nRun0:
+    calibMapFwd.update({iRun: [lambda x, a: x, [1], 'end']})
--- settings/y22StormCernT9_runList.py
+++
@@ -1,6 +0,0 @@
-# full run list
-# shape: {number: type} (all string)
-# select the run numbers or types to be opened with nRunToOpen...
-nRun0 = {
-    
-}
