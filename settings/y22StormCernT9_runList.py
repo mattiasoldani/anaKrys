9dd2f0f0b031528632e0c7c8a00e7cf0f2790211
mattiasoldani <mattiasoldani93@gmail.com>
2022-08-13 20:21:28 +0200

    online

--- settings/y22StormCernT9_runList.py
+++ settings/y22StormCernT9_runList.py
@@ -18,8 +18,13 @@ nRun0 = {
 
     "550239" : "Ir_Random",
     "550255" : "Ir_Random",
+    "550263" : "Ir_Random0",
     "550246" : "Ir_Axial",
     "550248" : "Ir_Axial",
+    "550261" : "Ir_Axial",
     "550256" : "Ir_AxisToandom_15mrad",
+    "550257" : "Ir_AxisToandom_7.5mrad",
+    "550258" : "Ir_AxisToandom_3.5mrad",
+    "550259" : "Ir_AxisToandom_3.5mrad",
     
 }
