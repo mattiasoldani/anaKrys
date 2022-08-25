2c46317bfc930d313ab531967c49ffcfc1027c7e
Mattia Soldani <mattiasoldani93@gmail.com>
2022-08-25 11:38:50 +0200

    online

--- settings/y22StormCernT9_runList.py
+++ settings/y22StormCernT9_runList.py
@@ -4,6 +4,15 @@
 nRun0 = {
     
     # direct
-    "550213" : "direct_Check"
+    "550227" : "direct_HolderCheck0",
+    "550228" : "direct_HolderCheck1",
+    "550230" : "direct_HolderCheck2",
+    
+    # Ir
+    "550237" : "Ir_ScanRot0",
+    "550238" : "Ir_ScanRot1",
+    "550239" : "Ir_CheckRandom0",
+    "550241" : "Ir_ScanCrad0",
+    "550241" : "Ir_ScanCrad0",
     
 }
