80ad6c4368bb5e381b0de3dd1ce3ac1af90b2254
Mattia Soldani <mattiasoldani93@gmail.com>
2022-11-22 02:15:57 +0100

    finalising analysis

--- settings/y18AxialCernH2_simulated_runList.py
+++ settings/y18AxialCernH2_simulated_runList.py
@@ -4,11 +4,13 @@
 nRun0 = {
     
     # PWO strip
-    "220621Random" : "PWOStrip_Random",
-    "220623Random" : "PWOStrip_Random",
     "220622Axial" : "PWOStrip_Axial",
     "220623Axial" : "PWOStrip_Axial",
+    "220621Random" : "PWOStrip_Random",
+    "220623Random" : "PWOStrip_Random",
     "221121RandomMagnOff" : "PWOStrip_Random_MagnetOff",
+    
+    "221122RandomMagnOffSmeared" : "PWOStrip_Random_MagnetOff_Smeared",
     
     # other runs
     "220623DirectMagnOff" : "PWOStrip_Direct_MagnetOff",
