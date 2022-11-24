f80cbdab005be49a0c3ffb5df09be297350a953b
Mattia Soldani <mattiasoldani93@gmail.com>
2022-11-24 09:48:58 +0100

    finalising analysis

--- settings/y18AxialCernH2_simulated_runList.py
+++ settings/y18AxialCernH2_simulated_runList.py
@@ -10,19 +10,22 @@ nRun0 = {
     "220623Random" : "PWOStrip_Random",
     "221121RandomMagnOff" : "PWOStrip_Random_MagnetOff",
     
-    "221122AxialSmeared002" : "PWOStrip_Axial_Smeared02",
-    "221122RandomSmeared002" : "PWOStrip_Random_Smeared02",
-    "221122RandomMagnOffSmeared002" : "PWOStrip_Random_MagnetOff_Smeared02",
+    "221122AxialSmeared" : "PWOStrip_Axial_Smeared02",
+    "221122RandomSmeared" : "PWOStrip_Random_Smeared02",
+    "221122RandomMagnOffSmeared" : "PWOStrip_Random_MagnetOff_Smeared02",
+    
+    "221124AxialSmeared0035" : "PWOStrip_Axial_Smeared035",
     
     # other runs
     "220623DirectMagnOff" : "PWOStrip_Direct_MagnetOff",
     "220623DirectMagnOn" : "PWOStrip_Direct_MagnetOn",
     
-    "221123DirectMagnOffSmeared002" : "PWOStrip_Direct_MagnetOff_Smeared02",
-    "221123DirectMagnOnSmeared002" : "PWOStrip_Direct_MagnetOn_Smeared02",
+    "221123DirectMagnOffSmeared" : "PWOStrip_Direct_MagnetOff_Smeared02",
+    "221123DirectMagnOnSmeared" : "PWOStrip_Direct_MagnetOn_Smeared02",
     
-    "221123DirectMagnOffSmeared005" : "PWOStrip_Direct_MagnetOff_Smeared05",
+    "221123DirectMagnOffSmeared005" : "PWOStrip_Direct_MagnetOff_Smeared05",  # used to correct experimental calibration
+    "221124Direct60MagnOffSmeared005" : "PWOStrip_Calib60_MagnetOff_Smeared05",  # used to correct experimental calibration
     
-    "221124Direct60MagnOffSmeared005" : "PWOStrip_Calib60_MagnetOff_Smeared05",
+    "221124DirectMagnOffSmeared0035" : "PWOStrip_Direct_MagnetOff_Smeared035",
     
 }
