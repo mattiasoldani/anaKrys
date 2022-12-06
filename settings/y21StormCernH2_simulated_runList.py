919be17a865d8a8da2ec4385dcba019f0fb3cd98
Mattia Soldani <mattiasoldani93@gmail.com>
2022-12-06 11:12:52 +0100

    finalising analysis

--- settings/y21StormCernH2_simulated_runList.py
+++ settings/y21StormCernH2_simulated_runList.py
@@ -8,17 +8,22 @@ nRun0 = {
     "220216RandomOld" : "PWO1X0_Random_Old",
     "220225Random" : "PWO1X0_Random",
     "220503Axial" : "PWO1X0_Axial",
-    "220504AxialMagnOff" : "PWO1X0_Axial_MagnetOff",  # yet to be generated
+    "220504AxialMagnOff" : "PWO1X0_Axial_MagnetOff",
+    
+    "221203RandomMagnOffSmeared0035" : "PWO1X0_Random_MagnetOff_Smeared035",
     
     # PWO 2 X0
     "2204292X0Random" : "PWO2X0_Random",
     "2204262X0Random" : "PWO2X0_Random",
-    "2205__2X0AxialMagnOff" : "PWO2X0_Axial_MagnetOff",  # yet to be generated
     "2205052X0Random" : "PWO2X0_Random",
     "2205052X0Axial" : "PWO2X0_Axial",
     "2205082X0Axial" : "PWO2X0_Axial",
     "2205102X0Axial" : "PWO2X0_Axial",
     "2205112X0Axial" : "PWO2X0_Axial",
+    
+    "2211292X0RandomMagnOffSmeared0035" : "PWO2X0_Random_MagnetOff_Smeared035",
+    "2212012X0RandomSmeared0035" : "PWO2X0_Random_Smeared035",
+    "2211302X0AxialSmeared0035" : "PWO2X0_Axial_Smeared035",
     
     # other runs
     "220222Calib120GeV" : "directBeam_120GeV",
@@ -28,4 +33,7 @@ nRun0 = {
     "220223Calib40GeV" : "directBeam_40GeV",
     "220222Calib20GeV" : "directBeam_20GeV",
     "220302DirectMagnOn" : "directBeam_MagnetOn",
+    
+    "221202DirectMagnOffSmeared0035" : "directBeam_120GeV_Smeared035",
+    "221202DirectMagnOnSmeared0035" : "directBeam_MagnetOn_Smeared035",
 }
