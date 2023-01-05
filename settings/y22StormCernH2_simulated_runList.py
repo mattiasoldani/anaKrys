0a2525b0d6af9e70aa3442c55a2c7a7a7777a8ea
Mattia Soldani <mattiasoldani93@gmail.com>
2023-01-06 00:28:28 +0100

    finalising analysis

---
+++ settings/y22StormCernH2_simulated_runList.py
@@ -0,0 +1,11 @@
+# full run list
+# shape: {number: type} (all string)
+# select the run numbers or types to be opened with nRunToOpen...
+nRun0 = {
+    
+    # PWO 1 X0
+    "2201204NoCrysSmeared0035" : "PWO1X0_Random_Old",
+    
+    # other runs
+
+}
