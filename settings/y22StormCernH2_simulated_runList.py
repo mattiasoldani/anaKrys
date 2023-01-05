76adefbb3a600f6afd8e61dac01179dc0a47257b
Mattia Soldani <mattiasoldani93@gmail.com>
2023-01-06 00:29:09 +0100

    finalising analysis

--- settings/y22StormCernH2_simulated_runList.py
+++ settings/y22StormCernH2_simulated_runList.py
@@ -4,8 +4,10 @@
 nRun0 = {
     
     # PWO 1 X0
+    "2201204NoCrysSmeared0035" : "NoCrys_Smeared035",
     "2201204NoCrysSmeared0035" : "PWO1X0_Random_Old",
     
     # other runs
-
+    "2201204NoCrysSmeared0035" : "PWO1X0_Random_Old",
+    
 }
