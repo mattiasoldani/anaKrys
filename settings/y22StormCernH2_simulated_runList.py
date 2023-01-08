f48f477d311294351eab16fea8eb44170ee1e5a0
Mattia Soldani <mattiasoldani93@gmail.com>
2023-01-08 10:39:41 +0100

    finalising analysis

--- settings/y22StormCernH2_simulated_runList.py
+++ settings/y22StormCernH2_simulated_runList.py
@@ -4,18 +4,18 @@
 nRun0 = {
     
     # PWO Uagliò
-    "2301xxAxialCorrFlat" : "PWOUaglio_Axial_Flat",
-    "2301xxAxialSmeared0025" : "PWOUaglio_Axial_Smeared025",
     "2301xxRandomCorrFlat" : "PWOUaglio_Random_Flat",
     "2301xxRandomSmeared0025" : "PWOUaglio_Random_Smeared025",
+    "2301xxAxialCorrFlat" : "PWOUaglio_Axial_Flat",
+    "2301xxAxialSmeared0025" : "PWOUaglio_Axial_Smeared025",
     
     #"221204RandomSmeared0035_NOEINIT" : "PWOUaglio_Random_Smeared035_OLD",
     #"230106RandomCorrFlat_NOEINIT" : "PWOUaglio_Random_Flat_OLD",
     #"221206WrongAxisSmeared0035_NOEINIT" : "PWOUaglio_Axial_Smeared035_OLD",
     
     # other runs
-    "230107NoCrysCorrFlat" : "NoCrys_Flat",
-    "2301xxNoCrysSmeared0025" : "NoCrys_Smeared025",
+    "230107NoCrysCorrFlat" : "NoCrys_Combo",
+    "230108NoCrysSmeared0025" : "NoCrys_Combo",
     
     #"221204NoCrysSmeared0035_NOEINIT" : "NoCrys_Smeared035_OLD",
     #"230106NoCrysCorrFlat_NOEINIT" : "NoCrys_Flat_OLD",
