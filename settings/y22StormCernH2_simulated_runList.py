dfd4eed0eb7abe51d50d0cb97dd38d1491149792
Mattia Soldani <mattiasoldani93@gmail.com>
2023-01-11 21:31:55 +0100

    finalising analysis

--- settings/y22StormCernH2_simulated_runList.py
+++ settings/y22StormCernH2_simulated_runList.py
@@ -5,11 +5,11 @@ nRun0 = {
     
     # PWO Uagliò
     "230108RandomCorrFlat" : "PWOUaglio_Random_Flat",
-    "230110RandomSmeared0025" : "PWOUaglio_Random_Smeared025_ComboEPi",
-    "230111PIONSRandomCorrFlat" : "PWOUaglio_Random_Smeared025_ComboEPi",
+    "230110RandomSmeared0025" : "PWOUaglio_Random_Smeared025_E",
+    "230111PIONSRandomSmeared0025" : "PWOUaglio_Random_Smeared025_Pi",
     "230109AxialCorrFlat" : "PWOUaglio_Axial_Flat",
-    "230109AxialSmeared0025" : "PWOUaglio_Axial_Smeared025_ComboEPi",
-    "230111PIONSAxialCorrFlat" : "PWOUaglio_Axial_Smeared025_ComboEPi",
+    "230109AxialSmeared0025" : "PWOUaglio_Axial_Smeared025_E",
+    "230111PIONSAxialSmeared0025" : "PWOUaglio_Axial_Smeared025_Pi",
     
     #"221204RandomSmeared0035_NOEINIT" : "PWOUaglio_Random_Smeared035_OLD",
     #"230106RandomCorrFlat_NOEINIT" : "PWOUaglio_Random_Flat_OLD",
@@ -17,8 +17,8 @@ nRun0 = {
     
     # other runs
     "230107NoCrysCorrFlat" : "NoCrys_Flat",
-    "230108NoCrysSmeared0025" : "NoCrys_Smeared025_ComboEPi",
-    "230111PIONSNoCrysSmeared0025" : "NoCrys_Smeared025_ComboEPi",
+    "230108NoCrysSmeared0025" : "NoCrys_Smeared025_E",
+    "230111PIONSNoCrysSmeared0025" : "NoCrys_Smeared025_Pi",
     
     #"221204NoCrysSmeared0035_NOEINIT" : "NoCrys_Smeared035_OLD",
     #"230106NoCrysCorrFlat_NOEINIT" : "NoCrys_Flat_OLD",
