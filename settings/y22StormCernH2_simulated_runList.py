a2643c33e52bf03874d4896370a523a51adac1e5
Mattia Soldani <mattiasoldani93@gmail.com>
2023-02-02 03:30:29 +0100

    finalising analysis

--- settings/y22StormCernH2_simulated_runList.py
+++ settings/y22StormCernH2_simulated_runList.py
@@ -3,51 +3,50 @@
 # select the run numbers or types to be opened with nRunToOpen...
 nRun0 = {
     
-    # PWO Uagliò - axial (right axis)
-    "230201AxialNEWSmeared0025" : "PWOUaglio_Axial_ComboBeam_NEWAXIS",  
-    
-    # PWO Uagliò - axial (wrong axis)
-    "230131AxialCorrFlat" : "PWOUaglio_Axial_ComboBeam_114GeV",  
-    "230130AxialSmeared0025" : "PWOUaglio_Axial_ComboBeam_114GeV",  
-    "230131PIONSAxialSmeared0025" : "PWOUaglio_Axial_ComboBeam_114GeV", 
-    
-    #"230xxxAxialCorrFlat_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",  
-    "230131AxialSmeared0025_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",  
-    #"230xxxPIONSAxialSmeared0025_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",
-    
-    "230129AxialSmeared0025OLD" : "PWOUaglio_Axial_ComboBeam_115GeV", 
+    # PWO Uagliò - axial
     "230109AxialCorrFlat" : "PWOUaglio_Axial_ComboBeam",  # "PWOUaglio_Axial_Flat",
     "230109AxialSmeared0025" : "PWOUaglio_Axial_ComboBeam",  # "PWOUaglio_Axial_Smeared025_E",
     "230111PIONSAxialSmeared0025" : "PWOUaglio_Axial_ComboBeam",  # "PWOUaglio_Axial_Smeared025_Pi",
     "230112PIONSAxialSmeared0025" : "PWOUaglio_Axial_ComboBeam",  # "PWOUaglio_Axial_Smeared025_Pi",
+    "230202AxialCorrFlat_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",  
+    "230131AxialSmeared0025_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",  
+    #"230xxxPIONSAxialSmeared0025_80GeV" : "PWOUaglio_Axial_ComboBeam_80GeV",
+    "230202AxialCorrFlatLonger" : "PWOUaglio_Axial_ComboBeam_TOBEADDED", # this should be PWOUaglio_Axial_ComboBeam when creating flat120INTRUTHISLONGERFLATANDPIONSANDLESSSMEARED
+    
+    "230201AxialNEWAPPARENTLY001CorrFlat" : "PWOUaglio_Axial_ComboBeam_NEWAXIS0", 
+    "230201AxialNEWAPPARENTLY100CorrFlat" : "PWOUaglio_Axial_ComboBeam_NEWAXIS1", 
+    #"2302xxAxialNEWAPPARENTLY100Smeared0025" : "PWOUaglio_Axial_ComboBeam_NEWAXIS",  
+    "230131AxialCorrFlat" : "PWOUaglio_Axial_ComboBeam_114GeV",  
+    "230130AxialSmeared0025" : "PWOUaglio_Axial_ComboBeam_114GeV",  
+    "230131PIONSAxialSmeared0025" : "PWOUaglio_Axial_ComboBeam_114GeV", 
+    "230129AxialSmeared0025OLD" : "PWOUaglio_Axial_ComboBeam_115GeV", 
     #"221206WrongAxisSmeared0035_NOEINIT" : "PWOUaglio_Axial_Smeared035_OLD",
     
     # PWO Uagliò - random
-    "230130RandomCorrFlat" : "PWOUaglio_Random_ComboBeam_114GeV",  
-    "230130RandomSmeared0025" : "PWOUaglio_Random_ComboBeam_114GeV",  
-    "230131PIONSRandomSmeared0025" : "PWOUaglio_Random_ComboBeam_114GeV",
-    
+    "230108RandomCorrFlat" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Flat",
+    "230110RandomSmeared0025" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Smeared025_E",
+    "230111PIONSRandomSmeared0025" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Smeared025_Pi",
     "230201RandomCorrFlat_80GeV" : "PWOUaglio_Random_ComboBeam_80GeV",  
     "230131RandomSmeared0025_80GeV" : "PWOUaglio_Random_ComboBeam_80GeV",  
     "230201PIONSRandomSmeared0025_80GeV" : "PWOUaglio_Random_ComboBeam_80GeV",
     
+    "230130RandomCorrFlat" : "PWOUaglio_Random_ComboBeam_114GeV",  
+    "230130RandomSmeared0025" : "PWOUaglio_Random_ComboBeam_114GeV",  
+    "230131PIONSRandomSmeared0025" : "PWOUaglio_Random_ComboBeam_114GeV",
     "230128RandomSmeared0025OLD" : "PWOUaglio_Random_ComboBeam_115GeV",
     "230129RandomSmeared0025NOPE" : "PWOUaglio_Random_ComboBeam_116p1GeV",
-    "230108RandomCorrFlat" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Flat",
-    "230110RandomSmeared0025" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Smeared025_E",
-    "230111PIONSRandomSmeared0025" : "PWOUaglio_Random_ComboBeam",  # "PWOUaglio_Random_Smeared025_Pi",
     #"221204RandomSmeared0035_NOEINIT" : "PWOUaglio_Random_Smeared035_OLD",
     #"230106RandomCorrFlat_NOEINIT" : "PWOUaglio_Random_Flat_OLD",
     
     # other runs
-    "230131NoCrysCorrFlat" : "NoCrys_ComboBeam_114GeV",
-    "230129RandomSmeared0025" : "NoCrys_ComboBeam_114GeV", 
-    "230131PIONSNoCrysSmeared0025" : "NoCrys_ComboBeam_114GeV",
-    
-    "230130NoCrysSmeared0025CHECK" : "NoCrys_ComboBeam_114GeV_ANOTHER",
     "230107NoCrysCorrFlat" : "NoCrys_ComboBeam",  # "NoCrys_Flat",
     "230108NoCrysSmeared0025" : "NoCrys_ComboBeam",  # "NoCrys_Smeared025_E",
     "230111PIONSNoCrysSmeared0025" : "NoCrys_ComboBeam",  # "NoCrys_Smeared025_Pi",
+    
+    "230131NoCrysCorrFlat" : "NoCrys_ComboBeam_114GeV",
+    "230129RandomSmeared0025" : "NoCrys_ComboBeam_114GeV", 
+    "230131PIONSNoCrysSmeared0025" : "NoCrys_ComboBeam_114GeV",
+    "230130NoCrysSmeared0025CHECK" : "NoCrys_ComboBeam_114GeV_ANOTHER",
     #"221204NoCrysSmeared0035_NOEINIT" : "NoCrys_Smeared035_OLD",
     #"230106NoCrysCorrFlat_NOEINIT" : "NoCrys_Flat_OLD",
 
