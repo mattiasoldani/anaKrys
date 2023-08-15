31d8c94d67c9789f22c42b7f30bd27b705784378
Mattia Soldani <mattiasoldani93@gmail.com>
2023-08-15 10:01:53 +0200

    tweaking broken local repository

---
+++ y23OreoCernT9_settings.py
@@ -0,0 +1,256 @@
+import os
+import importlib
+
+# importing run list from the corresponding settings file (in the same directory as this)
+#     --> argument 2 in import_module() (package) must be "settings" ("settings.test") when working with custom (test) data
+nRun0 = (importlib.import_module("."+os.path.basename(__file__).replace("settings", "runList").replace(".py", ""), package="settings")).nRun0
+
+########################################################################################################################
+# DATA STRUCTURE
+
+# ROOT tree or NumPy array name, string
+# mandatory with ROOT/NPZ, useless with ASCII files
+treeName = ""
+
+# descaling fraction, i.e. fraction of events to be processed (uniformly distributed along the run)
+# the lower is this value, the smaller the loaded dataset
+# minimum/maximum: 1 event per file/all the events in the file
+# dictionary -- shape: {runNumber (string): value (float)}
+# value range: any -- automatically set to 1e-12 (1) if <=0 (>1) (see succolib functions)
+# mandatory, but can be left empty --> value set to 1
+descFrac = {}
+
+# number of lines per event in the ASCII/NPZ files -- integer >0
+# see asciiMap for the variable list format
+# mandatory with ASCII/NPZ, useless with ROOT files
+nLinesEv = 1
+
+# map of the ASCII/NPZ file variables
+# list of strings -- the names must be entered in the list in the same order as the ASCII/NumPy table (left-to-right)
+# in case of multiple lines per event (nLinesEv > 1), follow the columns-then-rows order:
+#     (0, 0), ..., (0, nCol(0)), (1,0), ..., (1, nCol(1)), ...,  (nLines, 0), ..., (nLines, nCol(nLines))
+# mandatory with ASCII/NPZ, useless with ROOT files
+asciiMap = list()
+for i in [1, 0, 3, 2]: asciiMap.append("xRaw"+str(i))
+for i in [1, 0, 3, 2]: asciiMap.append("nStripHit"+str(i))
+for i in [1, 0, 3, 2]: asciiMap.append("nHit"+str(i))
+for i in range(16): asciiMap.append("digiBaseAll"+str(i))
+asciiMap.append("digiPHRawCherenkov")
+asciiMap.append("digiPHRawCaloFwd")
+asciiMap.append("digiPHRawAPC1")
+asciiMap.append("digiPHRawAPC0")
+asciiMap.append("digiPHRawEmpty0")
+asciiMap.append("digiPHRawEmpty1")
+asciiMap.append("digiPHRawEmpty2")
+asciiMap.append("digiPHRawEmpty3")
+for i in range(8): asciiMap.append("digiPHRawEmpty"+str(i+4))
+asciiMap.append("digiTimeCherenkov")
+asciiMap.append("digiTimeCaloFwd")
+asciiMap.append("digiTimeAPC1")
+asciiMap.append("digiTimeAPC0")
+asciiMap.append("digiTimeEmpty0")
+asciiMap.append("digiTimeEmpty1")
+asciiMap.append("digiTimeEmpty2")
+asciiMap.append("digiTimeEmpty3")
+for i in range(8): asciiMap.append("digiTimeEmpty"+str(i+4))
+asciiMap.append("xGonioRawRot")
+asciiMap.append("xGonioRawCrad")
+asciiMap.append("xGonioRawHorsa")
+asciiMap.append("xGonioRawHorsaBig")
+asciiMap.append("xGonioRawVersa")
+asciiMap.append("iSpill")
+asciiMap.append("iStep")
+asciiMap.append("iAEv")
+
+# map of the ROOT tree variables
+# dictionary -- shape: {newName: oldName} (all string)
+# oldName format: look into the raw ROOT tree for the variable names
+# if oldName refers to a multivariable branch, each element must be inserted individually
+# mandatory, but can be left empty --> no variable mapping
+treeMap = {}
+    
+# variables to mirror, i.e. var --> -var
+# has to be set run by run
+# dictionary -- shape: {run: [var]} (all string)
+# var format: the full dataframe variable name
+# mandatory, but can be skipped/filled with [] for some/all runs --> no variable mirroring for missing runs
+mirrorMap = {}
+    
+# 1st level data filters
+# dictionary -- shape: 
+# {var (string): 
+#     [
+#          [bIncl0 (bool), [low0, up0] (all float)], 
+#          [bIncl1, [low1, up1]], 
+#          ...
+#     ]
+# }
+# var format: the full dataframe variable name
+# selection applied independently on each var: 
+#     and/or of all the out-of-range/in-range values for all the (lowX, upX) ranges for which bInclX=False/True
+# mandatory, but can be left empty --> no filtering
+filterMap = {}
+for i in range(4): filterMap.update({"xRaw"+str(i): [[True, [-20, 20]]]})  # senseful data from input tracking layers
+
+########################################################################################################################
+# SETUP GEOMETRY & TRACKING
+
+# positions of the setup elements along the beam axis z
+# has to be set run by run
+# dictionary -- shape: {run (string): {element (string): z (float)}}
+# element format:
+#     "gonio" for the crystal (base)
+#     "caloFwd" for the forward calorimeter (either front or centre) (base)
+#     for tracking modules, use the part of the variable name following "xRaw" (base: 4/2 input/output layers)
+# mandatory, but can be skipped/filled partially for some/all runs --> all missing base positions set to 0
+z = {}
+for iRun in nRun0:
+    z.update({iRun: {
+        "0": 0,
+        "1": 0,
+        "2": 268,
+        "3": 268,
+        "gonio": 268 + 90,
+        "caloFwd": 268 + 90 + 85 + 666,
+    }})
+    
+# base tracking modules, i.e. 4 (2) in the input (output) stage
+# list of lists of strings -- shape: [[xIn0, yIn0, xIn1, yIn1], [xOut, yOut]]
+# for all the fields, insert the part of the variable name following "xRaw"
+# mandatory
+baseTrackingMap = [["0", "1", "2", "3"], ["2", "3"]]
+
+# raw input angle distribution centres for modules alignment
+# has to be set run by run
+# dictionary -- shape: {run (string): [thX, thY] (2 float or None)}
+# if None, the raw distributions are centered via their distrbution modes
+# set 0 not to apply any shift
+# mandatory for all the runs
+thInCentres = {}
+for iRun in nRun0:
+    thInCentres.update({iRun: [None, None]})
+    
+# raw output angle distribution centres for modules alignment
+# has to be set run by run
+# dictionary -- shape: {run (string): [thX, thY] (2 float or None)}
+# if None, the raw distributions are centered via their distrbution modes
+# set 0 not to apply any shift
+# mandatory for all the runs
+thOutCentres = {}
+for iRun in nRun0:
+    thOutCentres.update({iRun: [None, None]})        
+
+# aligned input angle range cut, centered around 0, boundaries excluded
+# has to be set run by run
+# dictionary -- shape: {run (string): cut}
+# cut format: 
+#     if length=1, the only value rCut is the radius of a circular cut
+#     if length=2, the 2 values [xCut, yCut] are the half-width values axes of the axes of an elliptical cut
+#     if length=4, the 4 values [xCutL, xCutR, yCutL, yCutR] are the boundaries of a rectangular cut
+# mandatory, but can be skipped for some/all runs --> no cut defined, i.e. boolean always True, in missing runs
+thInCut = {}
+for iRun in nRun0:
+    if "Axial" in nRun0[iRun]:
+        thInCut.update({iRun: [0.5e-3]})
+    elif "AxisToRandom" in nRun0[iRun]:
+        thInCut.update({iRun: [1.0e-3]})
+    else:
+        thInCut.update({iRun: [2.0e-3]})
+
+# crystal fiducial rectangle applied at the crystal longitudinal position z -- boundaries excluded
+# has to be set run by run
+# dictionary -- shape: {run (string): [xCut0, xCut1, yCut0, yCut1] (4 float)}
+# mandatory, but can be skipped for some/all runs --> no cut defined, i.e. boolean always True, in missing runs
+xCryCut = {}
+for iRun in nRun0:
+    if ("W1p5mm" in nRun0[iRun]):
+        xCryCut.update({iRun: [3.9, 4.7, 3.8, 4.6]})
+    elif ("W2p5mm" in nRun0[iRun]):
+        if (int(iRun)<680570):
+            xCryCut.update({iRun: [3.9, 4.7, 7.4, 8.1]})
+        else:
+            xCryCut.update({iRun: [3.9, 4.7, 5.4, 6.2]})
+    else:
+        xCryCut.update({iRun: [-10, 10, -10, 10]})
+
+# upper/lower limit for low/high output multiplicity selection (included)
+# has to be set run by run
+# dictionary -- shape: {run (string): [lowWindowCut, upWindowCut] (all float)}
+# mandatory, but can be skipped for some/all runs --> no cuts defined, i.e. booleans always True, in missing runs
+outMultCut = {}
+    
+########################################################################################################################
+# GONIOMETER
+
+# goniometer DOF to be paired to other variables
+# dictionary -- shape: {gonioVar (string): param}
+# gonioVar format: insert the part of the variable name following "xGonioRaw"
+# param format: [pairedVar (string), bShift (bool), scale (float)]
+# pairedVar (shifted via its mean if bShift=True) is multiplied to scale and added to gonioVar
+# scale can be negative to adjust relative verso
+# mandatory, but can be left empty --> no goniometer DOF pairing
+gonioMap = {}
+gonioMap = { 
+    "Rot": ["thIn1", False, -10**6],
+    "Crad": ["thIn0", False, 10**6],
+    "Horsa": ["xCry1", True, -10],
+    "HorsaBig": ["xCry1", True, -2*10],
+}
+
+########################################################################################################################
+# DIGITIZERS
+
+# PH (equalised) cut interval -- inner events kept, boundaries excluded
+# has to be set run by run
+# dictionary - shape: {run (string): {var (string), [inf, sup] (2 floats)}}
+# var format: insert the part of the variable name following "digiPHRaw"
+# mandatory, but can be skipped for some/all runs or for some/all channels within a single run
+#     --> no cuts defined, i.e. booleans always True, in missing runs/channels
+digiPHCut = {}
+for iRun in nRun0:
+    digiPHCut.update({iRun: {}})
+
+# time cut interval -- inner events kept, boundaries excluded
+# has to be set run by run
+# dictionary - shape: {run (string): {var (string), [inf, sup] (2 floats)}}
+# var format: insert the part of the variable name following "digiPHRaw"
+# mandatory, but can be skipped for some/all runs or for some/all channels within a single run
+#     --> no cuts defined, i.e. booleans always True, in missing runs/channels
+digiTimeCut = {}
+for iRun in nRun0:
+    digiPHCut.update({iRun: {"CaloFwd" : [0, 1000]}})
+    digiPHCut.update({iRun: {"APC0" : [0, 1000]}})
+    digiPHCut.update({iRun: {"APC1" : [0, 1000]}})
+
+# set of channels that are forward calorimeter channels
+# has to be set run by run
+# dictionary -- shape: {run: [var0, var1, ...]} (all string)
+# varX format: insert the part of the variable name following "digiPHRaw"
+# mandatory, but can be skipped for some/all runs --> forward calo. total PH and energy are set to NaN for those runs
+lsDigiChCaloFwd = {}
+for iRun in nRun0: lsDigiChCaloFwd.update({iRun: ["CaloFwd"]})
+
+# equalisation functions and parameters for channels to be equalised
+# has to be set run by run
+# dictionary -- shape: {run (string): {var (string): [func, param, 'end']}}
+# var format: insert the part of the variable name following "digiPHRaw"
+# func format: any function with
+#     - the 1st argument, with custom name, as the raw PH to be equalised -- will be inserted automatically
+#     - a succession of other N variables with custom names -- these will be the function parameters
+# param format: the list of the N parameters values for channel var, in the same order as in func arguments 1-to-N
+# the 'end' string (within apostrophes & the precise form ", 'end'") is just a flag needed for some printing
+# mandatory, but can be skipped/filled partially for some/all runs --> raw values are kept for missing channels
+equalMap = {}
+            
+# (total) forward calorimeter calibration function and parameters
+# has to be set run by run
+# dictionary -- shape: {run (string): [func, param, 'end']}
+# func format: any function with
+#     - the 1st argument, with custom name, as the PH to be calibrated -- will be inserted automatically
+#     - a succession of other N variables with custom names -- these will be the function parameters
+# param format: the list of the N parameters values, in the same order as in func arguments 1-to-N
+# the 'end' string (within apostrophes & the precise form ", 'end'") is just a flag needed for some printing
+# mandatory, but can be skipped for some/all runs --> forward calo. energy is set to NaN for those runs
+calibMapFwd = {}
+for iRun in nRun0:
+    calibMapFwd.update({iRun: [lambda x, a: x, [1], 'end']})
