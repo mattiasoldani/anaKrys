f7f31084cb25becdb9bbc088eb9310fc36ac1acc
mattiasoldani <mattiasoldani93@gmail.com>
2023-10-01 17:18:24 +0200

    creating new branch

---
+++ settings/y23OreoCernT9_2_runList.py
@@ -0,0 +1,52 @@
+# full run list
+# shape: {number: type} (all string)
+# select the run numbers or types to be opened with nRunToOpen...
+nRun0 = {
+    
+    # preliminary
+    "680467" : "test",
+    "680468" : "test",
+    "680470" : "test",
+    "680471" : "test",
+    
+    # W 1.5mm
+    "680514" : "W1p5mm_Random",  # also good for W2p5mm random - comment below if working with W2p5mm
+    "680516" : "W1p5mm_AxisSearch_0",
+    "680517" : "W1p5mm_AxisSearch_0",
+    "680518" : "W1p5mm_AxisSearch_0",
+    "680519" : "W1p5mm_AxisSearch_1",
+    "680520" : "W1p5mm_AxisSearch_1",
+    "680521" : "W1p5mm_AxisSearch_1",
+    "680522" : "W1p5mm_AxisSearch_1",
+    "680523" : "W1p5mm_AxisSearch_1",
+    "680526" : "W1p5mm_AxisSearch_2",
+    "680539" : "W1p5mm_AxisSearch_2",
+    "680550" : "W1p5mm_AxisSearch_2",
+    "680551" : "W1p5mm_AxisSearch_2",
+    "680552" : "W1p5mm_AxisSearch_2",
+    "680555" : "W1p5mm_Axial",
+    "680556" : "W1p5mm_Random",  # also good for W2p5mm random - comment below if working with W2p5mm
+    "680557" : "W1p5mm_AxisToRandom_8mrad",
+    "680560" : "W1p5mm_AxisToRandom_4mrad",
+    "680563" : "W1p5mm_AxisToRandom_16mrad",
+    "680564" : "W1p5mm_AxisToRandom_4mrad",
+    "680567" : "W1p5mm_Axial",
+    "680568" : "W1p5mm_Axial",
+    "680583" : "W1p5mm_Axial",
+    
+    "680570" : "W2p5mm_AxisSearch0",
+    "680571" : "W2p5mm_AxisSearch0",
+    "680572" : "W2p5mm_AxisSearch0",
+    "680573" : "W2p5mm_AxisSearch0",
+    "680574" : "W2p5mm_AxisSearch0",  # to be included in axis search 1 as well
+    "680575" : "W2p5mm_AxisSearch0",
+    "680576" : "W2p5mm_AxisSearch0",
+    "680577" : "W2p5mm_AxisSearch1",
+    "680578" : "W2p5mm_AxisSearch1",
+    "680579" : "W2p5mm_AxisSearch1",
+    "680580" : "W2p5mm_AxisSearch1",
+    #"680514" : "W2p5mm_Random",  # comment if working with W1p5mm
+    #"680556" : "W2p5mm_Random",  # comment if working with W1p5mm
+    "680581" : "W2p5mm_Axial",
+    
+}
